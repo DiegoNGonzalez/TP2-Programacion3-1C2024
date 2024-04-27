032ff08fbe2645f3a5329c3673c9f3d85640dfa5
Diegonzalez Dev <diegonzalezdev@gmail.com>
2024-04-27 16:55:50 -0300

    agregando DisplayName a categoria, imagen,y marca

--- TP-2/Dominio/Marca.cs
+++ TP-2/Dominio/Marca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace dominio
 {
     public class Marca
     {
+        [DisplayName("ID")]
         public int IDMarca { get; set; }
+        [DisplayName("Marca")]
         public string NombreMarca { get; set; }
 
         public override string ToString()
